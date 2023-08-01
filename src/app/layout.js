2dc0c3f311a6584493360631f2671c08661e31a2
Emon <emon.mhk69@gmail.com>
2023-08-01 09:53:43 +0600

    header footer done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import NavBar from '@/components/NavBar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +15,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
 
+        <NavBar></NavBar>
+
         {children}
+
+        <Footer></Footer>
         
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-        
+    <main className="container mx-auto p-4">
+        hqwg
     </main>
   )
 }
